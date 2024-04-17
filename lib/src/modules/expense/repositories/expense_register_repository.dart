20a4fae2f4cf3f90127eef87691e2aee885da39b
brunaplatt <111389173+brunaplatt@users.noreply.github.com>
2024-04-17 14:21:23 -0300

    commit

--- lib/src/modules/expense/repositories/expense_register_repository.dart
+++ lib/src/modules/expense/repositories/expense_register_repository.dart
@@ -3,7 +3,7 @@ import 'package:my_money_app/src/http/app_dio.dart';
 import 'package:my_money_app/src/modules/expense/model/expense_send_model.dart';
 
 class ExpenseRegisterRepository {
-  String baseURL = "http://192.168.5.133:3099/expense";
+  String baseURL = "http://192.168.31.96:3099/expense";
 
   Future<Response<Map<String, dynamic>>> sendData(
       ExpenseSendModel expenseData) async {
